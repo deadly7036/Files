f74788643b64e4cfdf98d0e70e733a9fd6554609
deadly7036 <116720515+deadly7036@users.noreply.github.com>
2023-10-24 19:18:29 +0530

    Update mongod.js

--- mongod.js
+++ mongod.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://deadly7036:deadly7036@cluster0.iezdyjg.mongodb.net/YouTube?retryWrites=true&w=majority");
 
 const userSchema = new mongoose.Schema({
      email:String,
