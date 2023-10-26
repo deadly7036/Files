e7fa97763e6ecb21c918e9a6c8969328b03a6b4a
deadly7036 <116720515+deadly7036@users.noreply.github.com>
2023-10-26 17:54:56 +0000

    Lvl 6

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const express = require("express");
 const app = express();
 const session = require('express-session')
+const findOrCreate = require('mongoose-findorcreate')
 const bodyParser = require("body-parser");
 const passport = require('passport');
 app.use(bodyParser.urlencoded({ extended: true }));
+const GoogleStrategy = require("passport-google-oauth20");
 app.use(express.json());
 app.use(express.static("public"));
 ;
 app.use(session({
-  secret:process.env['Mongo'],
+  secret: process.env['Mongo'],
   resave: false,
   saveUninitialized: false,
 }))
@@ -22,10 +24,41 @@ const User_ = require("./mongod.js");
 
 passport.use(User_.createStrategy());
 
-passport.serializeUser(User_.serializeUser());
-passport.deserializeUser(User_.deserializeUser());
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
 
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
 
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "https://nodejs-24.mashoaib.repl.co/auth/google/secrets"
+},
+  function(accessToken, refreshToken, profile, cb) {
+    User_.findOrCreate({ googleId: profile.id }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+))
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
+  });
 app.get("/", (req, res) => {
   res.render("home.ejs")
 })
@@ -35,16 +68,16 @@ app.get("/register", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login.ejs")
 })
-app.get("/logout",(req,res)=>{
-  req.logout((err)=>{
-    if(err) return err;
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) return err;
     res.redirect("/");
   });
-  
+
 })
 
-app.get("/secrets",(req,res)=>{
-  if(req.isAuthenticated()) {
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
     res.render("secrets.ejs")
   } else {
     res.render("login.ejs")
@@ -53,31 +86,31 @@ app.get("/secrets",(req,res)=>{
 
 
 
-  
+
 
 
 
 app.post('/register', async (req, res) => {
-  User_.register({username:req.body.username},req.body.password, function(err,user){
-    if(err) {
+  User_.register({ username: req.body.username }, req.body.password, function(err, user) {
+    if (err) {
       console.log(err)
     } else {
-      passport.authenticate('local')(req,res, function(){
+      passport.authenticate('local')(req, res, function() {
         res.redirect("/secrets")
       })
     }
   })
 });
 app.post('/login', async (req, res) => {
- const user = new User_({
-   username:req.body.username,
-   password: req.body.password
- })
-  req.login(user, function(err){
-    if(err){
+  const user = new User_({
+    username: req.body.username,
+    password: req.body.password
+  })
+  req.login(user, function(err) {
+    if (err) {
       console.log(err)
     } else {
-      passport.authenticate('local')(req,res, function(){
+      passport.authenticate('local')(req, res, function() {
         res.redirect("/secrets")
       })
     }
@@ -87,6 +120,6 @@ app.post('/login', async (req, res) => {
 
 
 
-app.listen("7500",()=>{
+app.listen("7500", () => {
   console.log("server is running");
 })
--- mongod.js
+++ mongod.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require('passport-local-mongoose');
-
-mongoose.connect("mongodb+srv://:@cluster0.iezdyjg.mongodb.net/YouTube?retryWrites=true&w=majority");
+const findOrCreate = require('mongoose-findorcreate')
+mongoose.connect("mongodb+srv://deadly7036:deadly7036@cluster0.iezdyjg.mongodb.net/YouTube?retryWrites=true&w=majority");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -9,8 +9,10 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type:String,Number
-  }
+  },
+  googleId:String
 });
+userSchema.plugin(findOrCreate);
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
